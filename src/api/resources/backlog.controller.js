7eca9e8af251fee65e4505d39f70a4103a425e2a
Stephen Williams <armando2002@gmail.com>
2018-09-16 14:12:41 -0700

    connected GET all and POST endpoints to MongoDB

--- src/api/resources/backlog.controller.js
+++ src/api/resources/backlog.controller.js
@@ -1,12 +1,16 @@
 // import Joi for easy valdidation
 import Joi from 'joi';
+// import Backlog model from backlog.model.js
+import Backlog from './backlog.model';
 
 export default {
 
-// create action for create
+// create action for creating a game in the backlog
     async create(req,res){
         // log request body
         console.log(req.body);
+        // use a try catch block to catch any exceptions and handle them
+        try{
         // use Joi to validate that object provided matches schema
         const schema = Joi.object().keys({
             title: Joi.string().required(),
@@ -21,11 +25,26 @@ export default {
         if(error && error.details){
             return res.status(400).json(error);
         }
-        return res.json(value);
+        // use the Joi value object with a promise
+        const backlogItem = await Backlog.create(value);
+        return res.json(backlogItem);
+        }
+        catch(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
     },
-// create action for get all
+// create action for get all games in the backlog
     async findAll(req,res){
-        return res.json({msg: 'Add findAll action and replace this'});
+        try{
+            // use a promise to find all games and return as a JSON object
+            const backlog = await Backlog.find();
+            return res.json(backlog);
+        }
+        catch(err){
+            console.error(err);
+            return res.status(500).send(err);
+        }
     },
 // create action for delete game by ID
     async deleteGame(req,res){
