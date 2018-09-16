1b9a397cbb5b04c4d7d88e243cc4c194f077135c
Stephen Williams <armando2002@gmail.com>
2018-09-16 13:16:28 -0700

    added schema and model

--- src/api/resources/backlog.model.js
+++ src/api/resources/backlog.model.js
@@ -1,1 +1,30 @@
 import mongoose from 'mongoose';
+
+// use destructuring to grab the Schema class from mongoose
+const {Schema} = mongoose;
+
+// schema for DB model based on wireframes
+const backlogSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title required']
+    },
+    platform: {
+        type: String
+    },
+    status: {
+        type: String
+    },
+    comments: {
+        type: String
+    },
+    dateAdded: {
+        type: String
+    },
+    lastPlayed: {
+        type: String
+    }
+})
+
+// export the 'Backlog' model using the backlogSchema
+export default mongoose.model('Backlog', backlogSchema);
