b213b2978aebb1611252d399e7c9b0d42a15a5a8
Stephen Williams <armando2002@gmail.com>
2018-09-18 21:00:54 -0700

    added mocha and chai

--- src/app.js
+++ src/app.js
@@ -21,12 +21,6 @@ app.use(logger('dev'));
 // for entries that hit the root, serve index.html
 app.use('/', express.static('public'));
 
-// COMMENTING THESE OUT FOR NOW, INSTEAD TRYING TO SERVE THE PUBLIC FOLDER ABOVE
-// for entries that hit '/' show a message
-//app.get('/', (req, res) => res.json({msg: 'This is the games backlog app.'}));
-// for entries that hit '/' use the indexRouter (ADD THIS LATER)
-// app.use('/', indexRouter);
-
 // for entries that hit '/api' use the backlogRouter (ADD THIS LATER)
 app.use('/api', restRouter);
 
