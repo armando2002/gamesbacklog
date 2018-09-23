9812b5c16b9f74611a86ab2b3a602a9ffe6cab7d
Stephen Williams <armando2002@gmail.com>
2018-09-23 14:23:47 -0700

    removed static game cards and added cards from DB via API

--- public/index.js
+++ public/index.js
@@ -1,16 +1,44 @@
 
-// gather list of games from API
-function getGames() {
-    let url = 'https://limitless-tor-81099.herokuapp.com/gamesapi';
-    const games = fetch(url)
-    .then((resp) => resp.json());
-    alert(games);
-    return games;
-}
-
-// test loading of page
-$(getGames);
 
 // create an element based off the game
+function generateGameElement(game) {
+    return `
+    <li>
+                    <div class="card">
+                        <img src="https://images.pexels.com/photos/163114/mario-luigi-figures-funny-163114.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                            alt="">
+                        <div class="card-content">
+                            <h3 id="card-title">${game.title}</h3>
+                            <p>${game.platform}</p>
+                            <p>${game.status}</p>
+                        </div>
+                    </div>
+                </li>
+    `
+}
 
 // for each game create an element and add to the page
+function generateGamesList(game) {
+    $.each(game, function(index, value) { 
+        let gameElement = generateGameElement(value);
+        $('.js-gameUl').append(gameElement);
+    });
+}
+
+
+
+
+// gather list of games from API and then call generateGamesList function
+function getGames() {
+    let url = 'https://limitless-tor-81099.herokuapp.com/gamesapi';
+    fetch(url).then(
+    function(resp){ return resp.json();}
+    ).then(
+        function(json) {
+            console.log(json);
+            generateGamesList(json);
+        }
+    )
+    }
+
+$(getGames);
