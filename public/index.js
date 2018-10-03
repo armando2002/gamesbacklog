9a229841044c95e0cefab8369bd586b4b7fa0b8c
Stephen Williams <armando2002@gmail.com>
2018-10-02 17:51:59 -0700

    adding hiddenid class to card ids

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ function generateGameElement(game) {
                             <p>Comments: ${game.comments}</p>
                             <p>Date Added: ${game.dateAdded}</p>
                             <p>Last Played: ${game.lastPlayed}</p>
-                            <p class="js-gameid">${game._id}</p>
+                            <p class="js-gameid hiddenid">${game._id}</p>
                             <p><button type="button" class="deletegamebutton btn">Delete Game</button></p>
                         </div>
                     </div>
